0db0bd2530a737451b37f8bac8c33a89564db535
CatsInCode <158318270+CatsInCode@users.noreply.github.com>
2026-03-10 20:18:41 +0300

    Add control-panel toggle to hide pacman rendering

--- pacman/game.py
+++ pacman/game.py
@@ -21,6 +21,7 @@ class Game:
         self.__restart_on_rotate = False
         self.__ghost_count = 4
         self.__show_paths = True
+        self.__render_pacmans = True
         self.__apply_display_mode()
         self.__clock = time.Clock()
 
@@ -66,6 +67,7 @@ class Game:
             scene_manager.reset(new_scene)
             new_scene.set_ghost_count(self.__ghost_count)
             new_scene.set_paths_visible(self.__show_paths)
+            new_scene.set_pacmans_visible(self.__render_pacmans)
             self.__apply_display_mode()
             return
         if isinstance(scene_manager.current, MenuScene):
@@ -102,6 +104,7 @@ class Game:
                 scene_manager.reset(new_scene)
                 new_scene.set_ghost_count(self.__ghost_count)
                 new_scene.set_paths_visible(self.__show_paths)
+                new_scene.set_pacmans_visible(self.__render_pacmans)
                 self.__apply_display_mode()
             elif cmd.name == "rotate_left":
                 self.__rotation = (self.__rotation - 90) % 360
@@ -115,10 +118,15 @@ class Game:
                 if isinstance(scene_manager.current, MainScene):
                     scene_manager.current.set_ghost_count(self.__ghost_count)
                     scene_manager.current.set_paths_visible(self.__show_paths)
+                    scene_manager.current.set_pacmans_visible(self.__render_pacmans)
             elif cmd.name == "show_paths":
                 self.__show_paths = bool(cmd.value)
                 if isinstance(scene_manager.current, MainScene):
                     scene_manager.current.set_paths_visible(self.__show_paths)
+            elif cmd.name == "render_pacmans":
+                self.__render_pacmans = bool(cmd.value)
+                if isinstance(scene_manager.current, MainScene):
+                    scene_manager.current.set_pacmans_visible(self.__render_pacmans)
             elif cmd.name == "fullscreen":
                 self.__fullscreen = bool(cmd.value)
                 self.__apply_display_mode()
